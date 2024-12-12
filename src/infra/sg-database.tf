93e66e141d6b662d3afe05da7f70f6f6e6b30f0d
Jean Carlos M.  da Silva <33285182+jcmdsbr@users.noreply.github.com>
2024-12-12 17:14:54 -0300

    Update sg-database.tf

--- src/infra/sg-database.tf
+++ src/infra/sg-database.tf
@@ -17,15 +17,3 @@ resource "aws_security_group" "security-group-database" {
   }
 }
 
-output "db_endpoint" {
-  value = aws_db_instance.db-tc-backends3.endpoint
-}
-
-output "db_username" {
-  value = var.db_username
-}
-
-output "db_password" {
-  value     = var.db_password
-  sensitive = true
-}
